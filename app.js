daea727ccb4796bf4ba226fed5e42efee33d7e35
Renganathan M <srirenganathan.87@gmail.com>
2026-07-29 12:29:06 +0530

    fixed syntax error

--- app.js
+++ app.js
@@ -1140,7 +1140,7 @@ const mantras = [
     gayatriDevanagari: "ॐ ज्ञानानन्दाय विद्महे ब्रह्मनिष्ठाय धीमहि तन्नो गुरुः प्रचोदयात्॥",
     gayatriIast: "oṃ jñānānandāya vidmahe brahmaniṣṭhāya dhīmahi tanno guruḥ pracodayāt ||",
     image: "assets/Gnanananda.jfif"
-  }
+  },
   {
   name: "Dattatreya",
   type: "guru",
